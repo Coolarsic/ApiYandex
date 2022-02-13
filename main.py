084dd51d41431f712cdab324d73d2120e9a8833f
goldbocman <hxfcndjd863@gmail.com>
2022-02-13 14:26:21 +0200

    added scale

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 from PIL import Image, ImageQt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.Qt import Qt
 from yandexmaps import get_coords
 
 SCREEN_SIZE = [600, 450]
@@ -13,12 +14,14 @@ SCREEN_SIZE = [600, 450]
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+        self.target_coordinates = 'Калининград, Советский Проспект, 192'
+        self.target_scale = 0.004
         self.getImage()
         self.initUI()
 
     def getImage(self):
-        coords = get_coords('Калининград, Советский Проспект, 192')
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(coords)}&spn=0.004,0.004&l=map"
+        coords = get_coords(self.target_coordinates)
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(coords)}&spn={self.target_scale},{self.target_scale}&l=map"
         response = requests.get(map_request)
 
         if not response:
@@ -38,6 +41,56 @@ class Example(QWidget):
         self.image.move(0, 0)
         self.image.resize(600, 450)
         self.image.setPixmap(QPixmap.fromImage(self.img))
+
+    def update_image(self):
+        self.image.setPixmap(QPixmap.fromImage(self.img))
+
+    def keyPressEvent(self, e):
+        print('--------')
+        print(self.target_scale)
+        if e.key() == Qt.Key_PageUp:
+            if self.target_scale == 45.824:
+                return
+            if 0.001 < self.target_scale < 0.007:
+                self.target_scale += 0.003
+            elif self.target_scale == 0.007:
+                self.target_scale += 0.005
+            elif self.target_scale == 0.012:
+                self.target_scale += 0.011
+            elif self.target_scale == 0.023:
+                self.target_scale += 0.022
+            elif self.target_scale == 0.045:
+                self.target_scale += 0.045
+            elif self.target_scale == 0.09:
+                self.target_scale += 0.089
+            elif self.target_scale == 0.179:
+                self.target_scale += 0.179
+            else:
+                self.target_scale *= 2
+        elif e.key() == Qt.Key_PageDown:
+            if self.target_scale < 0.003:
+                return
+            if 0.001 < self.target_scale < 0.007:
+                self.target_scale = 0.001
+            elif self.target_scale == 0.007:
+                self.target_scale -= 0.005
+            elif self.target_scale == 0.012:
+                self.target_scale -= 0.011
+            elif self.target_scale == 0.023:
+                self.target_scale -= 0.022
+            elif self.target_scale == 0.045:
+                self.target_scale -= 0.045
+            elif self.target_scale == 0.09:
+                self.target_scale -= 0.089
+            elif self.target_scale == 0.179:
+                self.target_scale -= 0.179
+            else:
+                self.target_scale /= 2
+        self.getImage()
+        self.update_image()
+        print(self.target_scale)
+        print('--------')
+        print()
 
 
 if __name__ == '__main__':
