f7b7d6a9241a17277e6c0b39da4350a356f84cb7
goldbocman <hxfcndjd863@gmail.com>
2022-02-13 15:52:09 +0200

    added changing layer type

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from io import BytesIO
 import requests
 from PIL import Image, ImageQt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
 from PyQt5.Qt import Qt
 from yandexmaps import get_coords
 
@@ -17,13 +17,13 @@ class Example(QWidget):
         self.target_place = 'Калининград, Советский Проспект, 192'
         self.target_coordinates = list(map(float, get_coords(self.target_place)))
         self.target_scale = 0.004
+        self.target_layer = 'map'
         self.getImage()
         self.initUI()
 
     def getImage(self):
-        print(self.target_coordinates)
         coords = map(str, self.target_coordinates)
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(coords)}&spn={self.target_scale},{self.target_scale}&l=map"
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(coords)}&spn={self.target_scale},{self.target_scale}&l={self.target_layer}"
         response = requests.get(map_request)
 
         if not response:
@@ -38,11 +38,40 @@ class Example(QWidget):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
 
-        ## Изображение
+        # Изображение
         self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(600, 450)
         self.image.setPixmap(QPixmap.fromImage(self.img))
+
+        # button
+        button = QPushButton('Схема', self)
+        button.move(SCREEN_SIZE[0] - 100, SCREEN_SIZE[1] - 120)
+        button.clicked.connect(self.set_layer_map)
+
+        # button
+        button2 = QPushButton('Спутник', self)
+        button2.move(SCREEN_SIZE[0] - 100, SCREEN_SIZE[1] - 90)
+        button2.clicked.connect(self.set_layer_sat)
+
+        # button
+        button3 = QPushButton('Гибрид', self)
+        button3.move(SCREEN_SIZE[0] - 100, SCREEN_SIZE[1] - 60)
+        button3.clicked.connect(self.set_layer_gib)
+
+    def set_layer_map(self):
+        self.target_layer = 'map'
+        self.update_image()
+
+    def set_layer_sat(self):
+        self.target_layer = 'sat'
+        self.update_image()
+
+    def set_layer_gib(self):
+        self.target_layer = 'sat,skl'
+        self.update_image()
+
+
 
     def update_image(self):
         self.getImage()
@@ -100,16 +129,12 @@ class Example(QWidget):
             self.key_page_down_process()
         elif e.key() == Qt.Key_Up:
             self.target_coordinates[1] += 0.002500
-            print('up')
         elif e.key() == Qt.Key_Left:
             self.target_coordinates[0] -= 0.002500
-            print('left')
         elif e.key() == Qt.Key_Right:
             self.target_coordinates[0] += 0.002500
-            print('right')
         elif e.key() == Qt.Key_Down:
             self.target_coordinates[1] -= 0.002500
-            print('down')
         self.update_image()
         # print(self.target_scale)
         # print('--------')
