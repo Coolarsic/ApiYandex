7ccffa95dda06f415ce3fc4656958cea9dd1a151
goldbocman <hxfcndjd863@gmail.com>
2022-02-13 15:20:51 +0200

    added move by keys up, down, left, right

--- main.py
+++ main.py
@@ -14,13 +14,15 @@ SCREEN_SIZE = [600, 450]
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        self.target_coordinates = 'Калининград, Советский Проспект, 192'
+        self.target_place = 'Калининград, Советский Проспект, 192'
+        self.target_coordinates = list(map(float, get_coords(self.target_place)))
         self.target_scale = 0.004
         self.getImage()
         self.initUI()
 
     def getImage(self):
-        coords = get_coords(self.target_coordinates)
+        print(self.target_coordinates)
+        coords = map(str, self.target_coordinates)
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={','.join(coords)}&spn={self.target_scale},{self.target_scale}&l=map"
         response = requests.get(map_request)
 
@@ -43,54 +45,75 @@ class Example(QWidget):
         self.image.setPixmap(QPixmap.fromImage(self.img))
 
     def update_image(self):
+        self.getImage()
         self.image.setPixmap(QPixmap.fromImage(self.img))
 
+    def key_page_up_process(self):
+        if self.target_scale == 45.824:
+            return
+        if 0.001 < self.target_scale < 0.007:
+            self.target_scale += 0.003
+        elif self.target_scale == 0.007:
+            self.target_scale += 0.005
+        elif self.target_scale == 0.012:
+            self.target_scale += 0.011
+        elif self.target_scale == 0.023:
+            self.target_scale += 0.022
+        elif self.target_scale == 0.045:
+            self.target_scale += 0.045
+        elif self.target_scale == 0.09:
+            self.target_scale += 0.089
+        elif self.target_scale == 0.179:
+            self.target_scale += 0.179
+        else:
+            self.target_scale *= 2
+
+    def key_page_down_process(self):
+        if self.target_scale < 0.003:
+            return
+        if 0.001 < self.target_scale < 0.007:
+            self.target_scale = 0.001
+        elif self.target_scale == 0.007:
+            self.target_scale -= 0.005
+        elif self.target_scale == 0.012:
+            self.target_scale -= 0.011
+        elif self.target_scale == 0.023:
+            self.target_scale -= 0.022
+        elif self.target_scale == 0.045:
+            self.target_scale -= 0.045
+        elif self.target_scale == 0.09:
+            self.target_scale -= 0.089
+        elif self.target_scale == 0.179:
+            self.target_scale -= 0.179
+        else:
+            self.target_scale /= 2
+
     def keyPressEvent(self, e):
-        print('--------')
-        print(self.target_scale)
+        # print('--------')
+        # print(self.target_scale)
+        good_keys = [Qt.Key_PageUp, Qt.Key_PageDown, Qt.Key_Up, Qt.Key_Left, Qt.Key_Right, Qt.Key_Down]
+        if e.key() not in good_keys:
+            return
         if e.key() == Qt.Key_PageUp:
-            if self.target_scale == 45.824:
-                return
-            if 0.001 < self.target_scale < 0.007:
-                self.target_scale += 0.003
-            elif self.target_scale == 0.007:
-                self.target_scale += 0.005
-            elif self.target_scale == 0.012:
-                self.target_scale += 0.011
-            elif self.target_scale == 0.023:
-                self.target_scale += 0.022
-            elif self.target_scale == 0.045:
-                self.target_scale += 0.045
-            elif self.target_scale == 0.09:
-                self.target_scale += 0.089
-            elif self.target_scale == 0.179:
-                self.target_scale += 0.179
-            else:
-                self.target_scale *= 2
+            self.key_page_up_process()
         elif e.key() == Qt.Key_PageDown:
-            if self.target_scale < 0.003:
-                return
-            if 0.001 < self.target_scale < 0.007:
-                self.target_scale = 0.001
-            elif self.target_scale == 0.007:
-                self.target_scale -= 0.005
-            elif self.target_scale == 0.012:
-                self.target_scale -= 0.011
-            elif self.target_scale == 0.023:
-                self.target_scale -= 0.022
-            elif self.target_scale == 0.045:
-                self.target_scale -= 0.045
-            elif self.target_scale == 0.09:
-                self.target_scale -= 0.089
-            elif self.target_scale == 0.179:
-                self.target_scale -= 0.179
-            else:
-                self.target_scale /= 2
-        self.getImage()
+            self.key_page_down_process()
+        elif e.key() == Qt.Key_Up:
+            self.target_coordinates[1] += 0.002500
+            print('up')
+        elif e.key() == Qt.Key_Left:
+            self.target_coordinates[0] -= 0.002500
+            print('left')
+        elif e.key() == Qt.Key_Right:
+            self.target_coordinates[0] += 0.002500
+            print('right')
+        elif e.key() == Qt.Key_Down:
+            self.target_coordinates[1] -= 0.002500
+            print('down')
         self.update_image()
-        print(self.target_scale)
-        print('--------')
-        print()
+        # print(self.target_scale)
+        # print('--------')
+        # print()
 
 
 if __name__ == '__main__':
